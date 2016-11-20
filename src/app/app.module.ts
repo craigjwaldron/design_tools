c243ded964604d316d9c8b67981139fe2fc11c6a
Craig <craigjwaldron@gmail.com>
2016-11-20 14:17:16 -0600

    folder structure and component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TypographyComponent } from './typography/typography.component';
+import { ColorsComponent } from './colors/colors.component';
+import { PhotosComponent } from './photos/photos.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypographyComponent,
+    ColorsComponent,
+    PhotosComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: WelcomeComponent },
+      { path: 'photos/:id', component: PhotosComponent },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
