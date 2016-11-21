1dce9c7fb3cb275fc206856f8c687efd6d42bd4a
Craig <craigjwaldron@gmail.com>
2016-11-20 21:02:54 -0600

    updated routing & added minor styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1 class="title">Design Tools</h1>
-    <nav>
-      <a routerLink="/photos" routerLinkActive="active">Photos</a>
-      <a routerLink="/typography" routerLinkActive="active">Typography</a>
-      <a routerLink="/colors" routerLinkActive="active">Colors</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.less']
 })
 export class AppComponent {
 
